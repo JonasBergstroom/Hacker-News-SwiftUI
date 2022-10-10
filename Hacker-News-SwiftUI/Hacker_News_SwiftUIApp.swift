f2fb189ca7b6f50c4a12fe1c1fc97c414eedde19
jonasbergstrom <jontrian97@gmail.com>
2022-10-10 17:56:01 +0200

    Initial Commit

---
+++ Hacker-News-SwiftUI/Hacker_News_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hacker_News_SwiftUIApp.swift
+//  Hacker-News-SwiftUI
+//
+//  Created by Jonas Bergström on 2022-10-10.
+//
+
+import SwiftUI
+
+@main
+struct Hacker_News_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
